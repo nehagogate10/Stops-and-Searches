a8ed56d410b008fb0129046dbbd058273d4e6714
nehagogate10 <50893732+nehagogate10@users.noreply.github.com>
2022-08-29 23:06:59 -0500

    Create update-crime.js

---
+++ flaskr/static/js/update-crime.js
@@ -0,0 +1,32 @@
+function update_crime(event) {
+    const url = "/updatecrime";
+
+    const data = {
+        'CrimeID': document.getElementById('crimecrimeid').value,
+        'CategoryCrime': document.getElementById('crimecategorycrime').value,
+        'LastOutcome': document.getElementById('crimelastoutcome').value,
+    };
+
+    console.log(data);
+
+    fetch("/updatecrime", {
+        "method": "POST",
+        "mode" : "cors",
+        "headers": {
+            "Content-Type": "application/json; charset=UTF-8"
+        },
+        "body": JSON.stringify(data)
+    })
+    .then(function(response) {
+        response.json().then((data) => {
+            console.log(data);
+            document.getElementById('message').innerHTML = data.message;
+            document.getElementById('message').classList.add("alert", "alert-success");
+        });
+    })
+    .catch(err => {
+        console.error(err);
+        document.getElementById('message').innerHTML = err.message;
+    });
+    return false;
+}
